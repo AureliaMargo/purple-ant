c422c9a7002a6177900cafb4735db08fab58a7d7
AureliaMargo <138791144+AureliaMargo@users.noreply.github.com>
2023-08-13 14:00:05 +0200

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,114 +1,126 @@
 
+const wrapper = document.querySelector(".wrapper");
+const boxes = gsap.utils.toArray(".box");
 
-(function() {
-  var fsSlider = new FsLibrary('.cms-feed-one')  
+const wrapper2 = document.querySelector(".low-scroll-line-follow");
+const boxes2 = gsap.utils.toArray(".follow-heading-and-gif-wrapper");
 
-  fsSlider.slider({
-    sliderComponent: '.instagram-slider', 
-    itemsPerSlide: 1  
-  })
-})();
+const loop = horizontalLoop(boxes, {paused: false,repeat: -1,});
 
-(function() {
-  var fsSliderTwo = new FsLibrary('.cms-feed-two') 
-  fsSliderTwo.slider({
-    sliderComponent: '.instagram-cards-slider', 
-    itemsPerSlide: 1  
-  })
-})();
+const loop2 = horizontalLoop2(boxes2, {paused: false,repeat: -1,});
 
-
-const target_1 = $(".nav-link");
-var position_target_1 = getElementPosition(target_1, true);
-
-ScrollTrigger.addEventListener("refreshInit", function () {
-  position_target_1 = getElementPosition(target_1, true);
-});
-
-gsap.registerPlugin(ScrollTrigger);
-
-const sections = [
-  {
-    trigger: ".section_about",
-    color: "#1c1326",
-  },
-  {
-    trigger: ".section_services",
-    color: "#1c1326",
-  },
-  {
-    trigger: ".section_blog",
-    color: "#1c1326",
-  },
-  {
-    trigger: ".section_follow",
-    color: "#1c1326",
-  },
-];
-
-sections.forEach((section) => {
-  gsap.to(".nav-link", {
-    scrollTrigger: {
-      trigger: section.trigger,
-      start: () => "top " + position_target_1,
-      end: () => "+=" + target_1.outerHeight() + " " + position_target_1,
-      scrub: 1,
-    },
-    color: section.color,
+function horizontalLoop(items, config) {
+  items = gsap.utils.toArray(items);
+  config = config || {};
+  let tl = gsap.timeline({repeat: config.repeat, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)}),
+    length = items.length,
+    startX = items[0].offsetLeft,
+    times = [],
+    widths = [],
+    xPercents = [],
+    curIndex = 0,
+    pixelsPerSecond = (config.speed || 1) * 40,
+    snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), 
+    totalWidth, curX, distanceToStart, distanceToLoop, item, i;
+  gsap.set(items, { 
+    xPercent: (i, el) => {
+      let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+      xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
+      return xPercents[i];
+    }
   });
-
-  gsap.to(".nav-link", {
-    scrollTrigger: {
-      trigger: section.trigger,
-      start: () => "bottom " + position_target_1,
-      end: () => "+=" + target_1.outerHeight() + " +=" + position_target_1,
-      scrub: 1,
-    },
-    color: "#dfcefd",
-    immediateRender: false,
-  });
-});
-
-function getElementPosition(element, bottom = false) {
-  let position = element.offset().top - $(window).scrollTop();
-  return !bottom ? position : position + element.outerHeight();
+  gsap.set(items, {x: 0});
+  totalWidth = items[length-1].offsetLeft + xPercents[length-1] / 50 * widths[length-1] - startX + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+  for (i = 0; i < length; i++) {
+    item = items[i];
+    curX = xPercents[i] / 100 * widths[i];
+    distanceToStart = item.offsetLeft + curX - startX;
+    distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+    tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
+      .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
+      .add("label" + i, distanceToStart / pixelsPerSecond);
+    times[i] = distanceToStart / pixelsPerSecond;
+  }
+  function toIndex(index, vars) {
+    vars = vars || {};
+    (Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); 
+    let newIndex = gsap.utils.wrap(0, length, index),
+      time = times[newIndex];
+    if (time > tl.time() !== index > curIndex) {
+      vars.modifiers = {time: gsap.utils.wrap(0, tl.duration())};
+      time += tl.duration() * (index > curIndex ? 1 : -1);
+    }
+    curIndex = newIndex;
+    vars.overwrite = true;
+    return tl.tweenTo(time, vars);
+  }
+  tl.next = vars => toIndex(curIndex+1, vars);
+  tl.previous = vars => toIndex(curIndex-1, vars);
+  tl.current = () => curIndex;
+  tl.toIndex = (index, vars) => toIndex(index, vars);
+  tl.times = times;
+  tl.progress(1, true).progress(0, true); 
+  if (config.reversed) {
+    tl.vars.onReverseComplete();
+    tl.reverse();
+  }
+  return tl;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const sectionCaseStudies = document.querySelector(".section_case-studies");
-  const purpleCircle = document.querySelector(".purple-circle");
-
-  function animateOnScroll() {
-    const sectionPosition = sectionCaseStudies.getBoundingClientRect();
-    const sectionTop = sectionPosition.top;
-    const sectionHeight = sectionPosition.height;
-    const viewportHeight = window.innerHeight;
-    const positionThreshold = viewportHeight * 0.5;
-
-    if (sectionTop - positionThreshold <= 0 && sectionTop + sectionHeight >= positionThreshold) {
-      purpleCircle.classList.add("animate-on-scroll");
-    } else {
-      purpleCircle.classList.remove("animate-on-scroll");
+function horizontalLoop2(items, config) {
+  items = gsap.utils.toArray(items);
+  config = config || {};
+  let tl = gsap.timeline({repeat: config.repeat, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)}),
+    length = items.length,
+    startX = items[0].offsetLeft,
+    times = [],
+    widths = [],
+    xPercents = [],
+    curIndex = 0,
+    pixelsPerSecond = (config.speed || 1) * 50,
+    snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), 
+    totalWidth, curX, distanceToStart, distanceToLoop, item, i;
+  gsap.set(items, { 
+    xPercent: (i, el) => {
+      let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+      xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 50 + gsap.getProperty(el, "xPercent"));
+      return xPercents[i];
     }
+  });
+  gsap.set(items, {x: 0});
+  totalWidth = items[length-1].offsetLeft + xPercents[length-1] / 100 * widths[length-1] - startX + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+  for (i = 0; i < length; i++) {
+    item = items[i];
+    curX = xPercents[i] / 100 * widths[i];
+    distanceToStart = item.offsetLeft + curX - startX;
+    distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+    tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
+      .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
+      .add("label" + i, distanceToStart / pixelsPerSecond);
+    times[i] = distanceToStart / pixelsPerSecond;
   }
-
-  animateOnScroll();
-
-  window.addEventListener("scroll", animateOnScroll);
-});
-
-      $(document).ready(()=>{
-            let isMenuAlreadyOpen = false;
-            $('.button.is-menu').on('click',()=>{
-                  $('body').css("overflow",isMenuAlreadyOpen?"auto":"hidden")
-                  isMenuAlreadyOpen = !isMenuAlreadyOpen
-            })
-      })
-      
-       $(document).ready(()=>{
-            let isMenuAlreadyOpen = false;
-            $('.button.is-close, .nav-link, .apla').on('click',()=>{
-                  $('body').css("overflow",isMenuAlreadyOpen?"auto":"visible")
-                  isMenuAlreadyOpen = !isMenuAlreadyOpen
-            })
-      })
+  function toIndex(index, vars) {
+    vars = vars || {};
+    (Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); 
+    let newIndex = gsap.utils.wrap(0, length, index),
+      time = times[newIndex];
+    if (time > tl.time() !== index > curIndex) {
+      vars.modifiers = {time: gsap.utils.wrap(0, tl.duration())};
+      time += tl.duration() * (index > curIndex ? 1 : -1);
+    }
+    curIndex = newIndex;
+    vars.overwrite = true;
+    return tl.tweenTo(time, vars);
+  }
+  tl.next = vars => toIndex(curIndex+1, vars);
+  tl.previous = vars => toIndex(curIndex-1, vars);
+  tl.current = () => curIndex;
+  tl.toIndex = (index, vars) => toIndex(index, vars);
+  tl.times = times;
+  tl.progress(1, true).progress(0, true); 
+  if (config.reversed) {
+    tl.vars.onReverseComplete();
+    tl.reverse();
+  }
+  return tl;
+}
